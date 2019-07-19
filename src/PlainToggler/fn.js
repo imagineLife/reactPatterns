a3783e14ad2b57fa72a9d912d3a93ca84e6a0ac4
Eric Laursen <ericjlaursen@gmail.com>
2019-07-19 06:29:53 -0400

    function && hook version

---
+++ src/PlainToggler/fn.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+//The toggler HTML content is in the toggler file
+import {Switch} from '../Switch/class'
+
+const Toggle = () => {
+  let [ on, setOn ] = React.useState(false)
+
+  const toggle = () => setOn(!on)
+    
+  return <Switch on={on} onClick={toggle} />
+}
+
+function Usage() {
+  return <Toggle />
+}
+Usage.title = 'Build Toggle'
+
+export { Toggle, Usage }
