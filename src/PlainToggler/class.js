7a0d3aca410b7ca9c3289160f99ea9939d15262d
Eric Laursen <ericjlaursen@gmail.com>
2019-07-19 06:25:20 -0400

    v1 of toggler

---
+++ src/PlainToggler/class.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+//The toggler HTML content is in the toggler file
+import {Switch} from '../Switch/class'
+
+class Toggle extends React.Component {
+  state = {on: false}
+  toggle = () => this.setState({on: !this.state.on})
+  render() {
+    const {on} = this.state
+    console.log('%c CUR STATE ON', 'background-color: brown; color: white;')
+    console.log(on)
+    
+    
+    return <Switch on={on} onClick={this.toggle} />
+  }
+}
+
+function Usage({
+  onToggle = (...args) => console.log('onToggle', ...args),
+}) {
+  return <Toggle onToggle={onToggle} />
+}
+Usage.title = 'Build Toggle'
+
+export { Toggle, Usage }
