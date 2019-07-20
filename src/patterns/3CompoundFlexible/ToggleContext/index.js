ff6d54b65fb9b62a41eda999c4579f56f2af6de1
Eric Laursen <ericjlaursen@gmail.com>
2019-07-20 15:32:58 -0400

    creates context class

--- src/patterns/3CompoundFlexible/ToggleContext/index.js
+++ src/patterns/3CompoundFlexible/ToggleContext/index.js
@@ -3,10 +3,25 @@ import React from 'react'
 //1 create context
 const ToggleContext = React.createContext()
 
-const ToggleProvider = (props) => {
-  return(<ToggleContext.Provider>
-    {props.children}
-  </ToggleContext.Provider>)
+class ToggleProvider extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      on: false
+    }
+
+    this.toggleFn = this.toggleFn.bind(this)
+  }
+
+  toggleFn(){
+    this.setState({on: !this.state.on})
+  }
+  render(){
+    return(<ToggleContext.Provider value={{on, toggleFn: this.toggleFn}}>
+      {props.children}
+    </ToggleContext.Provider>)
+  }
+  
 }
 
 export { ToggleContext, ToggleProvider }
